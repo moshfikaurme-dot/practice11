2e2a2aeb8e7350f084652704cd7fffb4de181faa
Moshfika Najnin <moshfikaurme@gmail.com>
2025-11-23 20:55:34 +0600

    Add do-while loop examples to print multiples of 3 up to 100 and even numbers up to 50

---
+++ Cclass7_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i = 3;
+ do{
+    printf("%d ",i); //1
+    i =i+3; //2
+ }
+ while(i<=100);
+printf("\n");
+ 
+int j = 2;
+ do{
+    printf("%d ",j); //1
+    j =j+2; //2
+ }
+ while(j<=50);
+}
